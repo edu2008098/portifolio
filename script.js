e59daddf46954bc655da3af5de86cc3584c162a8
edu2008098 <eduardo.kroll@escola.pr.gov.br>
2025-05-15 17:09:36 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,17 @@
   let num2;
   let resultado;
 
+  num1 = Number(window.prompt("digite aqui o primeiro numero:"));
+  num2 = Number(window.prompt("digite o segundo numero:"));
+
+  
+  resultado = num1 + num2;
+
+  parOuimpar = resultado % 2;
+
+  if(parOuimpar == 0) {
+      alert("O numero " + resultado + " é par");
+  } else {
+      alert("O numero " + resultado  + " é impar");
+  }
+  
