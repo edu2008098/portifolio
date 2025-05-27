e2cf7a60dba5c88b6ae0412e5aac39292b5edeca
edu2008098 <eduardo.kroll@escola.pr.gov.br>
2025-05-27 08:11:42 -0300

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,8 @@
+function somar(){
+    let num1 = Number(window.prompt("digite aqui o primeiro numero"));
+    let num2 = Number(window.prompt("digite o segundo numero"));
+
+    let resultado = num1 + num2;
+
+    alert("o resultado é" + resultado + ".");
+}
